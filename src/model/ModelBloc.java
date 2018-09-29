80e3f65c3ac9fc6d5a64ecb106a3fad4de4260a6
Agustin Monter Vera <1717110270@utectulancingo.edu.mx>
2018-09-29 16:28:34 +0200

    first commit

---
+++ src/model/ModelBloc.java
@@ -0,0 +1,42 @@
+
+package model;
+
+
+
+public class ModelBloc {
+    
+    private String leer;
+    private String guardar;
+    private String menu;
+    
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public String getLeer() {
+        return leer;
+    }
+
+    public void setLeer(String leer) {
+        this.leer = leer;
+    }
+
+    public String getGuardar() {
+        return guardar;
+    }
+
+    public void setGuardar(String guardar) {
+        this.guardar = guardar;
+    }
+
+    
+    
+    
+   
+   
+    
+}
